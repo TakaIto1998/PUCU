5b30d3601aab00371b1de9830c3d616c20d53053
fuhao050403 <fh459027716@gmail.com>
2020-05-05 21:16:04 +0900

    Add player shooting.

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+      public Transform cameraPos;
+
+      public ParticleSystem gunFire;
+
+      public string weaponName = "Glock";
+      public float weaponDamage = 10f;
+      public float weaponRange = 100f;
+
+      [SerializeField]
+      private LayerMask mask;
+
+      // Start is called before the first frame update
+      void Start()
+      {
+        
+      }
+
+      // Update is called once per frame
+      void Update()
+      {
+            if(Input.GetButtonDown("Fire1"))
+            {
+                  WeaponShoot();
+            }
+      }
+
+      void WeaponShoot()
+      {
+            gunFire.Play();
+
+            RaycastHit _hit;
+            if(Physics.Raycast(cameraPos.position, cameraPos.forward, out _hit, weaponRange, mask))
+            {
+
+                  Debug.Log("we hit" + _hit.collider.name);
+            }
+      }
+}
